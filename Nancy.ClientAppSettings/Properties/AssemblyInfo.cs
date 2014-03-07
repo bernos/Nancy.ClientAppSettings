a4839efcd7d43d19b565b052db80a8b68164cc7e
Brendan McMahon <bernos@gmail.com>
2014-03-07 23:49:27 +1100

    added nuget confs

--- Nancy.ClientAppSettings/Properties/AssemblyInfo.cs
+++ Nancy.ClientAppSettings/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Nancy.ClientAppSettings")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A base library to support passing settings from server side (ie. web.config) to client side.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Brendan McMahon")]
 [assembly: AssemblyProduct("Nancy.ClientAppSettings")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.1")]
+[assembly: AssemblyFileVersion("0.0.1")]
