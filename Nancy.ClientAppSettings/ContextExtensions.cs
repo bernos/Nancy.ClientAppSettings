33e4e300d1e077d5e1f4a3f53583bd8410b26680
Brendan McMahon <bernos@gmail.com>
2014-03-09 14:12:10 +1100

    Added NancyContext extensions method for retrieving ClientAppSettings

---
+++ Nancy.ClientAppSettings/ContextExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Nancy.ClientAppSettings
+{
+    public static class ContextExtensions
+    {
+        public static ClientAppSettings GetClientAppSettings(this NancyContext context)
+        {
+            var settings = context.Items[ClientAppSettings.ContextKey] as ClientAppSettings;
+
+            if (settings != null) return settings;
+            
+            settings = new ClientAppSettings();
+            context.Items[ClientAppSettings.ContextKey] = settings;
+
+            return settings;
+        }
+    }
+}
